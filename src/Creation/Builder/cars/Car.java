6d2c4ece2c8fd535618aec91421e9f02648980ef
10110Lucas <lucas_900azul@hotmail.com>
2026-05-01 03:52:51 -0300

    feat(builder): Implement product classes for the Builder pattern

---
+++ src/Creation/Builder/cars/Car.java
@@ -0,0 +1,67 @@
+package Creation.Builder.cars;
+
+import Creation.Builder.components.Engine;
+import Creation.Builder.components.GPSNavigator;
+import Creation.Builder.components.Transmission;
+import Creation.Builder.components.TripComputer;
+
+/**
+ * en-us: Car is a product class. <br>
+ * pt-br: Car é uma classe de produto.
+ */
+public class Car {
+
+    private CarType carType;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+    private double fuel = 0;
+
+    public Car(CarType carType, int seats, Engine engine, Transmission transmission,
+               TripComputer tripComputer, GPSNavigator gpsNavigator) {
+        this.carType = carType;
+        this.seats = seats;
+        this.engine = engine;
+        this.transmission = transmission;
+        this.tripComputer = tripComputer;
+        
+        if (this.tripComputer != null) {
+            this.tripComputer.setCar(this);
+        }
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public TripComputer getTripComputer() {
+        return tripComputer;
+    }
+
+    public GPSNavigator getGpsNavigator() {
+        return gpsNavigator;
+    }
+}
